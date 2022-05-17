c462015f77e22304c82b9169a883b8e1a2dffd76
Luhan Fernandes <fernandes.luhan18@gmail.com>
2022-05-17 10:40:13 -0300

    MetodosJavaSobrecarga

---
+++ src/metodos_java_sobrecarga/Main.java
@@ -0,0 +1,11 @@
+package metodos_java_sobrecarga;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Soma.somar(10, 20, 40);
+		
+	}
+	
+}
---
+++ src/metodos_java_sobrecarga/Soma.java
@@ -0,0 +1,16 @@
+package metodos_java_sobrecarga;
+
+public class Soma {
+
+	public static int somar(int numero1, int numero2) {
+		return numero1 + numero2;
+	}
+	
+	public static float somar(float numero1, float numero2) {
+		return numero1 + numero2;
+	}
+	
+	public static int somar(int numero1, int numero2, int numero3) {
+		return numero1 + numero2 + numero3;
+	}
+}
